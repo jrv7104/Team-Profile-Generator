fb6d83fee7ea1d179713cad969765a6cb7f00469
Jason Viox <jrv04@hotmail.com>
2021-07-14 10:33:30 -0500

    moved classes to directories, index pseudocode

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ const inquirer = require("inquirer");
 const jest = require("jest");
 
 //require class systems
-const Manager = require(Manager.test.js);
-const Employee = require(Employee.test.js);
-const Engineer = require(Engineer.test.js);
-const Intern = require(Intern.test.js);
+const Manager = require(./lib/Manager.test.js);
+const Employee = require(.lib/Employee.test.js);
+const Engineer = require(./lib/Engineer.test.js);
+const Intern = require(./lib/Intern.test.js);
 
+//setup function to write data to html
+
+//create inquire questions
+
+//take answers and write to html
