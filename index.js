364df9b1dca606a4186d2ec430b15eefd55a5585
Jason Viox <jrv04@hotmail.com>
2021-07-14 15:34:11 -0500

    Update class system locations

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const fs = require("fs")
 
 //require class systems
 const Manager = require("./lib/Manager.js");
-// const Employee = require(lib/Employee.js);
-// const Engineer = require(lib/Engineer.js);
-// const Intern = require(lib/Intern.js);
+const Employee = require("./lib/Employee.js");
+const Engineer = require("./lib/Engineer.js");
+const Intern = require("./lib/Intern.js");
 
 const employeeArr = []
 //setup function to write data to html
